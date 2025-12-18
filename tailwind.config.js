a8b2db89735590d358d431e3956d8a329e4dbd1e
James <sanjames.dev@gmail.com>
2025-12-19 03:16:35 +0530

    Fix Tailwind purge for production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+  "./index.html",
+  "./src/**/*.{js,jsx,ts,tsx}",
+],
 }
