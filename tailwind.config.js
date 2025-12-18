440808588848084eef45ac5a1850f12ff2bd65fe
James <sanjames.dev@gmail.com>
2025-12-19 03:35:25 +0530

    Fix Tailwind v4 PostCSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-  "./index.html",
-  "./src/**/*.{js,jsx,ts,tsx}",
-],
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
